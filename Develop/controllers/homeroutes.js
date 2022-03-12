7a14a6a1b7842f6b0d1b7856b655b056f382721b
Thomas Frohm <tfrohm11@mail.com>
2022-03-12 11:32:05 -0600

    firstcommit

---
+++ Develop/controllers/homeroutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {Project} = require("./../models")
+
+
+router.get('/', async (req, res) => {
+  try {
+      const projectData = await Project.findAll();
+      const projects = projectData.map(project => project.get({plain: true}));
+    res.render('homepage', {projects} );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
