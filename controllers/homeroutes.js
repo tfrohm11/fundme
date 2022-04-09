69969e8741922a55341a132e9358ea22dd3ae6cc
Thomas Frohm <tfrohm11@mail.com>
2022-04-09 12:17:32 -0500

    testcommit

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const {Project} = require("../models")
-
+const { Project } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-      const projectData = await Project.findAll();
-      const projects = projectData.map(project => project.get({plain: true}));
-    res.render('homepage', {projects} );
+    const projectData = await Project.findAll();
+    const projects = projectData.map((project) => project.get({ plain: true }));
+    res.render('homepage', { projects });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
--- models/Project.js
+++ models/Project.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Project extends Model {}
-
+okedoke;
 Project.init(
   {
     id: {
